962283f6c6826d0e2fac468aeb3e80ea735a03c1
Sudip Raj Kunwar <sudip.kunwar9898@gmail.com>
2024-06-01 18:34:11 +0545

    remove comments

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,3 @@
-// // index.js
-// const express = require("express");
-// const { sequelize } = require("./config/database");
-// // const bodyParser = require("body-parser");
-
-// const PORT = 3000;
-// const app = express();
-
-// // Import routes
-// const authRoutes = require("./routes/authRoutes");
-// const employeeRoutes = require("./routes/employeesRoutes");
-// // const leaveRoutes = require("./routes/leaveRoutes");
-// // const reportRoutes = require("./routes/reportRoutes");
-
-// // app.use(bodyParser.json());
-
-// // Use routes
-// app.use("/auth", authRoutes);
-// app.use("/employees", employeeRoutes);
-// // app.use("/leave", leaveRoutes);
-// // app.use("/report", reportRoutes);
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -32,14 +11,8 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
-// respond with "hello world" when a GET request is made to the homepage
-// GET method route
-app.get("/", (req, res) => {
-  res.send("GET request to the a");
-});
-
-app.use("/auth", authRoutes);
-app.use("/employee", employeeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/employee", employeeRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
